a0bb2c2f4411fef075b911465906dd43ac0a6e8d
sbshaon007 <sbshaon21@gmail.com>
2026-07-18 03:23:34 +0600

    Module 8 Js Practice tasks

--- task2.js
+++ task2.js
@@ -1,7 +1,7 @@
 // Task 2: Guess the result of "5" == 5 and "5" === 5 first, then write code to verify your guess.
 
-"5" == 5  //true
-"5" === 5 //false
+"5" == 5  // true.
+"5" === 5 // false.
 
 console.log("5" == 5);
 console.log("5" === 5);
