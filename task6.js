05447c2d16c6801c13dc827ac4133a87341ad863
sbshaon007 <sbshaon21@gmail.com>
2026-07-18 03:04:07 +0600

    Module 8 Js practice task

---
+++ task6.js
@@ -0,0 +1,20 @@
+let year1 = 2022;
+let year2 = 2024;
+
+if (year1 % 4 === 0) {
+    
+    console.log("Year1: It's leap year")
+}
+
+else {
+    console.log("Year1: Not leap year")
+}
+
+if (year2 % 4 === 0) {
+    
+    console.log("Year2: It's leap year")
+} 
+
+else {
+    console.log("Year2: Not leap year")
+}
