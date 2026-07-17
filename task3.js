d3ac7f1a316caf8f83e0dbd6dc019dc4ad77bc81
sbshaon007 <sbshaon21@gmail.com>
2026-07-18 03:24:08 +0600

    Module 8 Js Practice tasks

--- task3.js
+++ task3.js
@@ -1,4 +1,4 @@
-// Task:3 Create a variable isRaining (true/false). If it is true, print "Take an umbrella".
+// Task:3 Create a variable isRaining (true/false). If it is true, print "Take an umbrella"
 
 let isRaining = true;
 
