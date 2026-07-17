bee392d7efd00403d68a020af8d10e8d5bac6ec7
sbshaon007 <sbshaon21@gmail.com>
2026-07-18 03:22:21 +0600

    Module 8 Js Practice tasks

---
+++ task10.js
@@ -0,0 +1,16 @@
+// Task 10: Build a simple login system — print "Login successful" only if both username and password are correct.
+
+let usernameCorrect = true;
+let passwordCorrect = true;
+
+if (usernameCorrect === true && passwordCorrect === true) {
+    
+    console.log("Login successful")
+
+} 
+
+// else {
+    
+//     console.log("Login unsuccessful")
+
+// }
---
+++ task8.js
@@ -0,0 +1,9 @@
+// Task 8: Using age and hasTicket, print "Entry allowed" if age is above 18 AND the person has a ticket (use &&).
+
+let age = 20;
+let hasTicket = true;
+
+if (age >= 18 && hasTicket === true) {
+    
+    console.log("Entry allowed");
+}
