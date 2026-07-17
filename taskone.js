b089890c5264f425fb13c891e17583933ac2d422
sbshaon007 <sbshaon21@gmail.com>
2026-07-18 02:11:42 +0600

    Module 8 Js practice task

---
+++ taskone.js
@@ -0,0 +1,12 @@
+let price1 = 100;
+let price2 = 50;
+
+
+console.log(price1 == price2);
+console.log(price1 === price2);
+console.log(price1 != price2);
+console.log(price1 !== price2);
+console.log(price1 > price2);
+console.log(price1 < price2);
+console.log(price1 >= price2);
+console.log(price1 <= price2);
